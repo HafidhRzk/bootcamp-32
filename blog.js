9d213e9365c6f5809b527f22e930e22997623acb
Hafidh Rizky <hafidhrzk@gmail.com>
2022-03-08 01:39:59 +0700

    ver 1

---
+++ blog.js
@@ -0,0 +1,248 @@
+let blogs = []
+
+function addBlog() {
+
+    let title = document.getElementById('input-blog-title').value;
+    let content = document.getElementById('input-blog-content').value;
+    let image = document.getElementById('input-blog-image').files[0];
+    let startDate = document.getElementById('start-date').value
+    let endDate = document.getElementById('end-date').value
+
+    let duration = endDate - startDate
+  
+    image = URL.createObjectURL(image)
+
+    let blog = {
+        title: title,
+        content: content,
+        image: image,
+        duration: duration,
+        postedAt: new Date()
+    }
+
+    blogs.push(blog)
+
+    renderBlog()
+}
+
+function renderBlog() {
+
+    let blogContainer = document.getElementById('contents')
+    console.log(blogs)
+
+    blogContainer.innerHTML = 
+    `<div class="blog-list-item">
+      <div class="blog-image">
+          <img src="assets/blog-img.jpg" alt="" />
+      </div>
+      <div class="item-content">
+          <h3>
+            <a style="text-decoration:none; color:black" 
+            href="blog-detail.html" target="_blank">Dumbways Mobile App 2021</a>
+          </h3>
+          <p style="color: grey">Post Date : 12 January 2022 14:30 WIB</p>
+          <p style="color: grey">Durasi : 3 Month</p>
+          <div style="text-align: right; color: grey;">3 Month Ago</div>
+          <p>App that used for Dumbways student, it was deployed and can downloaded on play store. 
+            happy download</p>
+      </div>
+      <div class="item-logo">
+          <i class="fa-brands fa-google-play"></i>
+          <i class="fa-brands fa-android"></i>
+          <i class="fa-brands fa-java"></i>
+      </div>
+      <div class="item-button">
+          <a href="#" class="btn-edit">Edit</a>
+          <a href="#" class="btn-post">Delete</a>
+      </div>
+    </div>
+    <div class="blog-list-item">
+      <div class="blog-image">
+          <img src="assets/blog-img.jpg" alt="" />
+      </div>
+      <div class="item-content">
+          <h3>
+            <a style="text-decoration:none; color:black" 
+            href="blog-detail.html" target="_blank">Dumbways Mobile App 2021</a>
+          </h3>
+          <p style="color: grey">Post Date : 12 January 2022 14:30 WIB</p>
+          <p style="color: grey">Durasi : 3 Month</p>
+          <div style="text-align: right; color: grey;">3 Month Ago</div>
+          <p>App that used for Dumbways student, it was deployed and can downloaded on play store. 
+            happy download</p>
+      </div>
+      <div class="item-logo">
+          <i class="fa-brands fa-google-play"></i>
+          <i class="fa-brands fa-android"></i>
+          <i class="fa-brands fa-java"></i>
+      </div>
+      <div class="item-button">
+          <a href="#" class="btn-edit">Edit</a>
+          <a href="#" class="btn-post">Delete</a>
+      </div>
+    </div>
+    <div class="blog-list-item">
+      <div class="blog-image">
+          <img src="assets/blog-img.jpg" alt="" />
+      </div>
+      <div class="item-content">
+          <h3>
+            <a style="text-decoration:none; color:black" 
+            href="blog-detail.html" target="_blank">Dumbways Mobile App 2021</a>
+          </h3>
+          <p style="color: grey">Post Date : 12 January 2022 14:30 WIB</p>
+          <p style="color: grey">Durasi : 3 Month</p>
+          <div style="text-align: right; color: grey;">3 Month Ago</div>
+          <p>App that used for Dumbways student, it was deployed and can downloaded on play store. 
+            happy download</p>
+      </div>
+      <div class="item-logo">
+          <i class="fa-brands fa-google-play"></i>
+          <i class="fa-brands fa-android"></i>
+          <i class="fa-brands fa-java"></i>
+      </div>
+      <div class="item-button">
+          <a href="#" class="btn-edit">Edit</a>
+          <a href="#" class="btn-post">Delete</a>
+      </div>
+    </div>
+    <div class="blog-list-item">
+      <div class="blog-image">
+          <img src="assets/blog-img.jpg" alt="" />
+      </div>
+      <div class="item-content">
+          <h3>
+            <a style="text-decoration:none; color:black" 
+            href="blog-detail.html" target="_blank">Dumbways Mobile App 2021</a>
+          </h3>
+          <p style="color: grey">Post Date : 12 January 2022 14:30 WIB</p>
+          <p style="color: grey">Durasi : 3 Month</p>
+          <div style="text-align: right; color: grey;">3 Month Ago</div>
+          <p>App that used for Dumbways student, it was deployed and can downloaded on play store. 
+            happy download</p>
+      </div>
+      <div class="item-logo">
+          <i class="fa-brands fa-google-play"></i>
+          <i class="fa-brands fa-android"></i>
+          <i class="fa-brands fa-java"></i>
+      </div>
+      <div class="item-button">
+          <a href="#" class="btn-edit">Edit</a>
+          <a href="#" class="btn-post">Delete</a>
+      </div>
+    </div>
+    <div class="blog-list-item">
+      <div class="blog-image">
+          <img src="assets/blog-img.jpg" alt="" />
+      </div>
+      <div class="item-content">
+          <h3>
+            <a style="text-decoration:none; color:black" 
+            href="blog-detail.html" target="_blank">Dumbways Mobile App 2021</a>
+          </h3>
+          <p style="color: grey">Post Date : 12 January 2022 14:30 WIB</p>
+          <p style="color: grey">Durasi : 3 Month</p>
+          <div style="text-align: right; color: grey;">3 Month Ago</div>
+          <p>App that used for Dumbways student, it was deployed and can downloaded on play store. 
+            happy download</p>
+      </div>
+      <div class="item-logo">
+          <i class="fa-brands fa-google-play"></i>
+          <i class="fa-brands fa-android"></i>
+          <i class="fa-brands fa-java"></i>
+      </div>
+      <div class="item-button">
+          <a href="#" class="btn-edit">Edit</a>
+          <a href="#" class="btn-post">Delete</a>
+      </div>
+    </div>`
+
+    for(let i = 0; i < blogs.length; i++) {
+        blogContainer.innerHTML += 
+        `<div class="blog-list-item">
+          <div class="blog-image">
+              <img src="${blogs[i].image}" alt="" />
+          </div>
+          <div class="item-content">
+              <h3>
+                <a style="text-decoration:none; color:black" 
+                href="blog-detail.html" target="_blank">${blogs[i].title}</a>
+              </h3>
+              <p style="color: grey">Post Date : ${getFullTime(blogs[i].postedAt)}</p>
+              <p style="color: grey">Durasi : ${blogs[i].duration} Day</p>
+              <div style="text-align: right; color: grey;">${getDistanceTime(blogs[i].postedAt)}</div>
+              <p>${blogs[i].content}</p>
+          </div>
+          <div class="item-logo">
+          <i class="fa-brands fa-google-play"></i>
+          <i class="fa-brands fa-android"></i>
+          <i class="fa-brands fa-java"></i>
+          </div>
+          <div class="item-button">
+              <a href="#" class="btn-edit">Edit</a>
+              <a href="#" class="btn-post">Delete</a>
+          </div>
+        </div>`
+    }
+}
+
+let month = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+]
+
+
+function getFullTime(time) {
+  let date = time.getDate()
+  let monthIndex = time.getMonth()
+  let year = time.getFullYear()
+  let hour = time.getHours()
+  let minute = time.getMinutes()
+
+  let fullTime = `${date} ${month[monthIndex]} ${year} ${hour}:${minute} WIB`
+
+  return fullTime
+}
+
+function getDistanceTime(time) {
+  let timeNow = new Date()
+  let timeBlog = new Date(time)
+
+  let distance = timeNow - timeBlog
+
+  let dayDistance = Math.floor(distance / (24 * 60 * 60 * 1000))
+
+  if(dayDistance != 0){
+    return dayDistance + ' day ago'
+  }else {
+    let hourDistance = Math.floor(distance / (60 * 60 * 1000))
+    
+    if(hourDistance != 0) {
+      return hourDistance + ' hours ago'
+    }else {
+      let minuteDistance = Math.floor(distance / (60 * 1000))
+
+      if(minuteDistance != 0){
+        return minuteDistance + ' minutes ago'
+      } else {
+        let secondsDistance = Math.floor(distance / 1000)
+
+        return secondsDistance + ' second ago'
+      }
+    }
+  }
+}
+
+setInterval(function(){
+  renderBlog()
+}, 1000)
